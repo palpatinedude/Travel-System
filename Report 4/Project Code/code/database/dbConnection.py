56a17a2bfa8f8ea7204b0c6405a56f12fbae6ccb
Marianthi Thodi <anthoula_thodi@hotmail.gr>
2024-05-28 21:10:35 +0300

    eee

--- Report 4/Project Code/code/database/dbConnection.py
+++ Report 4/Project Code/code/database/dbConnection.py
@@ -14,10 +14,3 @@ def create_connection():
             return connection
     except Error as e:
         print(f"Error: {e}")
-        return None
-'''
-def close_connection(connection):
-    if connection.is_connected():
-        connection.close()
-        print("MySQL connection closed")
-'''
